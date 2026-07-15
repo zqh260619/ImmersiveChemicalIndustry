4fc9b1ecb20e45a3d93bd7ef8e108b31ce7153ed
zqh260619 <xb200808192023@outlook.com>
2026-07-15 17:00:34 +0800

    更改了一些参数

---
+++ src/main/java/io/github/zqh260619/immersivechemicalindustry/ImmersiveChemicalIndustryClient.java
@@ -0,0 +1,10 @@
+package io.github.zqh260619.immersivechemicalindustry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImmersiveChemicalIndustryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
