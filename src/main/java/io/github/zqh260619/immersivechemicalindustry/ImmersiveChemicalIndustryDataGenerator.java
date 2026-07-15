1a31cbc90ab148a766c5e7bef48d51e066d38d30
zqh260619 <xb200808192023@outlook.com>
2026-07-15 14:30:02 +0800

    Initial commit

---
+++ src/main/java/io/github/zqh260619/immersivechemicalindustry/ImmersiveChemicalIndustryDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.zqh260619.immersivechemicalindustry;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ImmersiveChemicalIndustryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
